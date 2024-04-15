c1f8aac6c3bc60af489f70f119d1faef39910f74
ios <87903312+iosrouter@users.noreply.github.com>
2024-04-14 19:41:59 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-cp usr/lib/libimagepicker.dylib "${THEOS}/lib."
+cd $(dirname $(echo $0))
+mkdir -p ${PWD}/{usr/lib/,var/jb/usr/lib/}
+make clean package THEOS_PACKAGE_SCHEME=rootless
+find .theos/_/ -name "*.dylib" -exec cp -a {} ${PWD}/var/jb/usr/lib/ \;
+mkdir -p ${THEOS}/lib/iphone/rootless/
+cp -a ${PWD}/var/jb/usr/lib/libimagepicker.dylib ${THEOS}/lib/iphone/rootless
+make clean package 
+find .theos/_/ -name "*.dylib" -exec cp -a {} ${PWD}/usr/lib/ \;
+cp -a ${PWD}/usr/lib/libimagepicker.dylib ${THEOS}/lib/
